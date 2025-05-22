cd5fe9a7b5f2deb1dac61333ac049c64a8055f6c
Godisouleymane <ssabiougodi@gmail.com>
2025-05-22 17:17:35 +0100

    premier commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    int limit;
+    int a = 0, b = 1, next = 0;
+
+
+    scanf("%d", &limit);
+
+    printf("Fibonacci : ");
+
+    while (next < limit) {
+        printf("%d ", next);
+        a = b;
+        b = next;
+        next = a + b;
+    }
+
+    printf("\n");
+    return 0;
+}
 
