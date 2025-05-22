1df02f1c7efa13a800e47b166eda0987e2df00e9
Godisouleymane <ssabiougodi@gmail.com>
2025-05-22 17:12:12 +0100

    premier commit

--- celsius_to_fahrenheit.c
+++ celsius_to_fahrenheit.c
@@ -4,11 +4,8 @@ int main(void)
 {
     float celsius, fahrenheit;
 
-    // Demander la température en Celsius
-    printf("Entrez la température en °C : ");
     scanf("%f", &celsius);
 
-    // Conversion en Fahrenheit
     fahrenheit = (celsius * 9 / 5) + 32;
 
     printf("%.1f°C équivaut à %.1f°F\n", celsius, fahrenheit);
