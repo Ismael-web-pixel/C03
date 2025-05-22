10664d749de1d3c763304644424d9d721847c4fb
Godisouleymane <ssabiougodi@gmail.com>
2025-05-22 16:15:32 +0100

    premier commit

--- reverse_number.c
+++ reverse_number.c
@@ -4,15 +4,14 @@ int main(void)
 {
     int number, reversed = 0;
 
-    // Lire le nombre depuis l'utilisateur
-    printf("Entrez un nombre : ");
+    
     scanf("%d", &number);
 
     while (number != 0)
     {
-        int digit = number % 10;          // Obtenir le dernier chiffre
-        reversed = reversed * 10 + digit; // Construire le nombre inversé
-        number /= 10;                     // Supprimer le dernier chiffre
+        int digit = number % 10;          
+        reversed = reversed * 10 + digit; 
+        number /= 10;                     
     }
 
     printf("Nombre inversé : %d\n", reversed);
